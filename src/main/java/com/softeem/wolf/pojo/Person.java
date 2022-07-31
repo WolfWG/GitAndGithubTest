c607df0ed7e4ac69ae0d5c33f9327bfb051f111e
zhoule <3342968446@qq.com>
2022-07-31 15:20:11 +0800

    添加了一个Person实体类

---
+++ src/main/java/com/softeem/wolf/pojo/Person.java
@@ -0,0 +1,20 @@
+package com.softeem.wolf.pojo;
+
+import java.io.Serializable;
+
+/**
+ * Created by 苍狼
+ * Time on 2022-07-31
+ */
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = -1949070094437141876L;
+
+    private String name;
+
+    private String password;
+
+    private Integer age;
+
+
+}
