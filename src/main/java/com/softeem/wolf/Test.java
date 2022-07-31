279e76e185821386d320adbf6a655cfc479e2492
zhoule <3342968446@qq.com>
2022-07-31 14:47:10 +0800

    测试一下

---
+++ src/main/java/com/softeem/wolf/Test.java
@@ -0,0 +1,11 @@
+package com.softeem.wolf;
+
+/**
+ * Created by 苍狼
+ * Time on 2022-07-31
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("世界, 你好");
+    }
+}
